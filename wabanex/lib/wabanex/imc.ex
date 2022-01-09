91d2b8c43bbba6a3347ccf7803711397123d1bb2
Lucas <13675790+lschiavini@users.noreply.github.com>
2022-01-09 16:44:05 -0300

    Creates map from database

--- wabanex/lib/wabanex/imc.ex
+++ wabanex/lib/wabanex/imc.ex
@@ -1,5 +1,30 @@
 defmodule Wabanex.IMC do
   def calculate(filename) do
-    File.read(filename)
+    filename
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, content}) do
+    content
+    |> String.split("\n")
+    |> Enum.map(fn line -> parse_line(line) end)
+    |> Enum.into%{}
+  end
+
+  defp handle_file({:error, _reason}) do
+    {:error, "Error while opening file"}
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split(",")
+    |> List.update_at(1, &String.to_float/1)
+    |> List.update_at(2, &String.to_float/1)
+    |> calculate_imc()
+    |> IO.inspect()
+  end
+
+  defp calculate_imc([name, height, weight]), do: {name, weight / (height * height)}
+
 end
